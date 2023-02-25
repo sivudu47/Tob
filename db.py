6aadd2d23964deb57fc7b6abd203652f62565a1c
siva.anandha <siva.anandha@cloudera.com>
2023-02-25 16:45:48 +0530

    Few More

--- db.py
+++ db.py
@@ -10,23 +10,26 @@ class DBHelper:
     # customer table
     customer_id_field_name = "CustomerID"
     customer_table = "dbo.Customer"
-    phone_number_field_name = ""
-    customer_name_field_name = ""
+    phone_number_field_name = "ContactNumber"
+    customer_name_field_name = "Name"
 
     # bill table
     bill_table = "dbo.CustomerBill"
     balance_amount_field_name = "BalanceAmount"
-    invoice_field_name = ""
-    transaction_date_field_name = ""
-    total_amount = ""
+    paid_amount_field_name = "PaidAmount"
+    invoice_field_name = "SalesInvoiceNo"
+    transaction_date_field_name = "SalesDate"
+    total_amount = "BillAmount"
     limit_thresold = 5
 
     #product_table
-    product_name_field_name = ""
-    product_price_field_name = ""
-    product_table = ""
+    product_name_field_name = "ProductName"
+    product_price_field_name = "DealerPrice"
+    product_table = "dbo.Product"
+    product_id_field_name = "ProductID"
 
     c_id_phone_no_name = {}
+    product_details = {}
 
     #variables = customer_id_field_name, phone_number_field_name,customer_name_field_name customer_table
     get_cid_and_phone_numbers_and_name = "SELECT {}, {}, {} FROM {}"
@@ -36,19 +39,21 @@ class DBHelper:
     get_customer_id = "SELECT {} FROM {} where {} like '%{}%'"
 
     # get_pending_amount
-    # variables balance_amount_field_name,  bill_table, customer_id_field_name, customer_id, customer_id_field_name, customer_id_field_name
+    # variables balance_amount_field_name,  bill_table, customer_id_field_name, customer_id, customer_id_field_name, balance_amount_field_name
     get_pending_amount = "SELECT CAST(SUM({}) AS INTEGER) FROM {} where {} = {} GROUP BY {} HAVING CAST(SUM({}) AS INTEGER) > 0"
 
     # get_last_five_transactions
     # variables transaction_date_field_name, invoice_field_name, total_amount, balance_amount_field_name|, bill_table, customer_id_field_name, customer_id, transaction_date_field_name, limit_thresold
-    get_last_five_transactions = "SELECT {}, {}, {}, {} FROM {} WHERE {} = {} ORDER BY {} LIMIT {}"
+    get_last_five_transactions = "SELECT {}, {}, {}, {}, {} FROM {} WHERE {} = {} ORDER BY {} LIMIT {}"
 
     # get_last_five_unpaid_transactions
-    # variables transaction_date_field_name, invoice_field_name, total_amount, balance_amount_field_name| , bill_table, customer_id_field_name, customer_id, balance_amount_field_name, transaction_date_field_name, limit thresold
-    get_last_five_unpaid_transactions_query = "SELECT {}, {}, {}, {} FROM {} WHERE {} = {} AND CAST({}) > 0 ORDER BY {} LIMIT {}"
+    # variables transaction_date_field_name, invoice_field_name, total_amount, balance_amount_field_name ,datediff, bill_table, customer_id_field_name, customer_id, balance_amount_field_name, transaction_date_field_name
+    get_last_five_unpaid_transactions_query = "SELECT CAST({} AS VARCHAR), {}, CAST({} AS INTEGER), CAST({} AS INTEGER),  abs(DATEDIFF(day,GETDATE(), {}))  FROM {} WHERE {} = {} AND CAST({} AS INTEGER) > 0 AND  abs(DATEDIFF(day,GETDATE(), {})) >= {} ORDER BY {}"
 
-    #variables = product_name_field_name, product_price_field_name, product_table
-    get_product_details_query = "SELECT {}, {} FROM {}"
+    #variables = product, product_name_field_name, product_price_field_name, product_table
+    get_product_details_query = "SELECT {}, {}, CAST({} AS INTEGER) FROM {}"
+
+    #sample = "SELECT CAST(SalesDate AS VARCHAR), SalesInvoiceNo, CAST(BillAmount AS INTEGER), CAST(BalanceAmount AS INTEGER),  abs(DATEDIFF(day, getdate(), SalesDate))  FROM dbo.CustomerBill WHERE CustomerID = 1 AND CAST(BalanceAmount AS INTEGER) > 0  ORDER BY SalesDate"
 
     def __init__(self):
         pass
@@ -67,29 +72,72 @@ class DBHelper:
             #CHECK TYPES OF dictionaries AND HANDLE NONE, HANDLE MULTIPLE PHONE NUMBERS, HANDLE NATION CODE
             c_id = row[0]
             p_no = row[1]
+            c_name = row[2]
+            if "," in p_no:
+                p_no = p_no.split(',')[0]
             if row[0] not in self.c_id_phone_no_name:
-                phone_and_name = row[1]
-                phone_and_name.append(row[2])
-                self.c_id_phone_no_name[c_id] = p_no
+                phone_and_name = []
+                phone_and_name.append(p_no)
+                phone_and_name.append(c_name)
+                self.c_id_phone_no_name[c_id] = phone_and_name
         conn.close()
+        #debugging pls remove
+        """for i in self.c_id_phone_no_name:
+            print(i,self.c_id_phone_no_name[i])"""
         return self.c_id_phone_no_name
 
-    def get_last_five_unpaid_transactions(self,c_id):
+    #returns a dict of bill details of a customer with the customer id as key
+    def get_unpaid_transactions(self,c_id, no_of_days):
         conn = pyodbc.connect('Driver={};'
                               'Server={};'
                               'Database={};'
                               'Trusted_Connection=yes;'.format(self.driver_name, self.server_name, self.database_name))
         cursor = conn.cursor()
-        cursor.execute(self.get_last_five_unpaid_transactions_query).format(self.transaction_date_field_name, self.invoice_field_name, self.total_amount, self.balance_amount_field_name , self.bill_table, self.customer_id_field_name, c_id, self.balance_amount_field_name, self.transaction_date_field_name, self.limit_thresold)
-        unpaid_transactions = []
+        #debugging
+        #print(self.get_last_five_unpaid_transactions_query.format(self.transaction_date_field_name, self.invoice_field_name, self.total_amount, self.balance_amount_field_name, self.transaction_date_field_name, self.bill_table, self.customer_id_field_name, c_id, self.balance_amount_field_name,self.transaction_date_field_name, self.no_of_days, self.transaction_date_field_name))
+        cursor.execute(self.get_last_five_unpaid_transactions_query.format(self.transaction_date_field_name, self.invoice_field_name, self.total_amount, self.balance_amount_field_name, self.transaction_date_field_name, self.bill_table, self.customer_id_field_name, c_id, self.balance_amount_field_name,self.transaction_date_field_name, no_of_days, self.transaction_date_field_name))
+        unpaid_transactions = {}
+        #import pdb;pdb.set_trace()
         while 1:
             row = cursor.fetchone()
             if not row:
                 break
-            unpaid_transactions.append(row[0] + ',' + row[1] + ',' + row[2], + ',' + row[3])
-        #form_formatted_string and return, use better data structure
+            transaction_date = row[0]
+            invoice_no = row[1]
+            total_amount = row[2]
+            balance_amount = row[3]
+            datediff = row[4]
+            unpaid_transactions[invoice_no] = []
+            temp_details_list= []
+            temp_details_list.append(transaction_date)
+            temp_details_list.append(total_amount)
+            temp_details_list.append(balance_amount)
+            temp_details_list.append(datediff)
+
+            unpaid_transactions[invoice_no] = temp_details_list        #form_formatted_string and return, use better data structure
         conn.close()
         return unpaid_transactions
+
+        # returns the unpaid amount as INT
+    def get_unpaid_amount(self, c_id):
+        conn = pyodbc.connect('Driver={};'
+                              'Server={};'
+                              'Database={};'
+                              'Trusted_Connection=yes;'.format(self.driver_name, self.server_name,
+                                                               self.database_name))
+        cursor = conn.cursor()
+        #debugging
+        #print(self.balance_amount_field_name, self.bill_table, self.customer_id_field_name, c_id, self.customer_id_field_name, self.balance_amount_field_name)
+        #print(self.get_pending_amount.format(self.balance_amount_field_name, self.bill_table, self.customer_id_field_name, c_id, self.customer_id_field_name, self.balance_amount_field_name))
+        cursor.execute(self.get_pending_amount.format(self.balance_amount_field_name, self.bill_table, self.customer_id_field_name, c_id, self.customer_id_field_name, self.balance_amount_field_name))
+
+        unpaid_amount = 0
+        row = cursor.fetchone()
+        if row:
+            unpaid_amount = row[0]
+        # form_formatted_string and return, use better data structure
+        conn.close()
+        return unpaid_amount
 
 
     def get_product_details(self):
@@ -98,14 +146,21 @@ class DBHelper:
                               'Database={};'
                               'Trusted_Connection=yes;'.format(self.driver_name, self.server_name, self.database_name))
         cursor = conn.cursor()
-        cursor.execute(self.get_product_details_query.format(self.product_name_field_name, self.product_price_field_name, self.product_table))
-        product_details = []
+        cursor.execute(self.get_product_details_query.format(self.product_id_field_name,self.product_name_field_name, self.product_price_field_name, self.product_table))
         while 1:
             row = cursor.fetchone()
             if not row:
                 break
-            product_details.append(row[0] + ',' + row[1] + ',' + row[2], + ',' + row[3])
+            product_id = row[0]
+            product_name = row[1]
+            product_value = row[2]
+            product_details_list = []
+            product_details_list.append(product_name)
+            product_details_list.append(product_value)
+            self.product_details[product_id] = product_details_list
         conn.close()
+        # debugging pls remove
+        """for i in self.product_details:
+            print(i, self.product_details[i])"""
         #Form formatted message here itself and return , Handle none values
-        return product_details
-
+        return self.product_details
--- sss.py
+++ sss.py
@@ -2,121 +2,170 @@ from telethon import TelegramClient
 from telethon.tl.types import InputPhoneContact
 from telethon.tl.functions.contacts import ImportContactsRequest
 from telethon import functions, types
-#from db import DBHelper
+from db import DBHelper
 import random, string
+import time
+from ratelimit import limits, sleep_and_retry
 
-#db = DBHelper()
+
+
+
+db = DBHelper()
 
 # using now() to get current time
 
 
 #siva's
-name = "raju"
+name = "arya"
 
-api_id = ""
+api_id = "26993628"
 
-api_hash = ""
+api_hash = "d216bb3832283b111204838893050869"
 
 company_string = "MOS"
 
-owner_phone_no = "7010942886"
+sleep_time = 0
+
+content_length = 3000
+
+no_of_days = 5
+
+owner_phone_number = "9092363875"
 
 owner_name = "sivudu"
+
+CALLS = 5
+
+RATE_LIMIT = 60
+
 
 random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
 client = TelegramClient(name, api_id, api_hash)
 #TELEGRAM API ALONE GIVES CONTACT NUMBER WITHOUT NATION CODE
 
 #async def main():
+@sleep_and_retry
+@limits(calls=CALLS, period=RATE_LIMIT)
 async def main():
-    dialogs = await client.get_dialogs()
-    customer_id_phone_no_mappings = {1: ['9944193999', 'S.SANKARI'], 2: ['', 'MAHALAKSHMI TEA SHOP'], 3: ['', 'GOMATHY TEA  STALL'], 4: ['', 'MURUGAN IDLY SHOP'], 5: ['', 'NAMATHUVAR'], 6: ['9489120804', 'SEETHA LAKSHMI'], 7: ['9962382864', 'Balamurugan'], 8: ['9894143470', 'A.R.karthik'], 9: ['', 'SS.chinadurai '], 10: ['', 'karthick cibi'], 11: ['', 'KARUNAS HOTEL'], 12: ['', 'SUDALAI NARAYANAN'], 13: ['', 'SS.CHINNADURAI'], 14: ['', 'P.SENTHIL KUMAR'], 15: ['', 'TITANIC SHANMUGAM'], 16: ['', 'KS TEA STALL'], 20: ['9994496601', 'VADAKAM AKKA '], 21: ['', 'boss'], 22: ['9442054356', 'P.SENTHIL KUMAR'], 23: ['', 'VIKAS'], 24: ['', 'SB HOMES'], 25: ['', 'VARADHARAJAN AUDITOR'], 26: ['', 'RAJAGOPAL '], 27: ['9487868207', 'Sri Rengan '], 28: ['9487868207', 'Sri Rengan'], 29: ['8778064043', 'P.Dinesh'], 30: ['', 'Kathar Store'], 31: ['', 'A3J Mess'], 32: ['', 'TMB அக்கா '], 33: ['', 'R.KRISHNA NADAR'], 34: ['', 'dfgsdg']}
-    #db.find_all_customer_ids_and_phone_numbers()
-    customer_bill_details = {1: [29689006, {'A72/300120': ['Jan 30 2020  7:16PM', 60, 60], 'B14/310120': ['Jan 31 2020  1:21PM', 112, 112], 'B28/190223': ['Feb 19 2023 12:04AM', 492, 492], 'B29/190223': ['Feb 19 2023 12:06AM', 328, 328], 'B30/190223': ['Feb 19 2023 12:09AM', 2688014, 2688014], 'B31/190223': ['Feb 19 2023 12:27AM', 27000000, 27000000]}]}
+    """dialogs = await client.get_dialogs()
+    customer_id_phone_no_mappings = db.find_all_customer_ids_and_phone_numbers()#{1: ['9944193999', 'S.SANKARI'], 2: ['', 'MAHALAKSHMI TEA SHOP'], 3: ['', 'GOMATHY TEA  STALL'], 4: ['', 'MURUGAN IDLY SHOP'], 5: ['', 'NAMATHUVAR'], 6: ['9489120804', 'SEETHA LAKSHMI'], 7: ['9962382864', 'Balamurugan'], 8: ['9894143470', 'A.R.karthik'], 9: ['', 'SS.chinadurai '], 10: ['', 'karthick cibi'], 11: ['', 'KARUNAS HOTEL'], 12: ['', 'SUDALAI NARAYANAN'], 13: ['', 'SS.CHINNADURAI'], 14: ['', 'P.SENTHIL KUMAR'], 15: ['', 'TITANIC SHANMUGAM'], 16: ['', 'KS TEA STALL'], 20: ['9994496601', 'VADAKAM AKKA '], 21: ['', 'boss'], 22: ['9442054356', 'P.SENTHIL KUMAR'], 23: ['', 'VIKAS'], 24: ['', 'SB HOMES'], 25: ['', 'VARADHARAJAN AUDITOR'], 26: ['', 'RAJAGOPAL '], 27: ['9487868207', 'Sri Rengan '], 28: ['9487868207', 'Sri Rengan'], 29: ['8778064043', 'P.Dinesh'], 30: ['', 'Kathar Store'], 31: ['', 'A3J Mess'], 32: ['', 'TMB அக்கா '], 33: ['', 'R.KRISHNA NADAR'], 34: ['', 'dfgsdg']}
+    customer_bill_details = {}#{1: [29689006, {'A72/300120': ['Jan 30 2020  7:16PM', 60, 60], 'B14/310120': ['Jan 31 2020  1:21PM', 112, 112], 'B28/190223': ['Feb 19 2023 12:04AM', 492, 492], 'B29/190223': ['Feb 19 2023 12:06AM', 328, 328], 'B30/190223': ['Feb 19 2023 12:09AM', 2688014, 2688014], 'B31/190223': ['Feb 19 2023 12:27AM', 27000000, 27000000]}]}
     #{}  # key - cid values - list [1- Unpaid amount, 2- Bill details with bill id as key
-    """today_product_details = db.get_product_details()
-    for customer_id in customer_id_phone_no_mappings:
+    today_product_details = db.get_product_details()
+    # remove in original system"""
+    customer_bill_details = {1: [29689006, {'A72/300120': ['Jan 30 2020  7:16PM', 60, 60, 1121], 'B14/310120': ['Jan 31 2020  1:21PM', 112, 112, 1120], 'B28/190223': ['Feb 19 2023 12:04AM', 492, 492, 5], 'B29/190223': ['Feb 19 2023 12:06AM', 328, 328, 5], 'B30/190223': ['Feb 19 2023 12:09AM', 2688014, 2688014, 5], 'B31/190223': ['Feb 19 2023 12:27AM', 27000000, 27000000, 5]}], 6: [0, {}], 7: [0, {}], 8: [0, {}], 20: [3090, {'A365/190220': ['Feb 19 2020  1:20PM', 2995, 2995, 1101], 'A642/240220': ['Feb 24 2020 12:29PM', 95, 95, 1096]}], 22: [0, {}], 27: [0, {}], 28: [0, {}], 29: [0, {}]}
+    customer_id_phone_no_mappings = {1: ['9994242534', 'Sandy'], 2: ['', 'MAHALAKSHMI TEA SHOP'], 3: ['', 'GOMATHY TEA  STALL'], 4: ['', 'MURUGAN IDLY SHOP'], 5: ['', 'NAMATHUVAR'], 6: ['8056766011', 'Sindhuja'], 7: ['9962382864', 'Balamurugan'], 8: ['9894143470', 'A.R.karthik'], 9: ['', 'SS.chinadurai '], 10: ['', 'karthick cibi'], 11: ['', 'KARUNAS HOTEL'], 12: ['', 'SUDALAI NARAYANAN'], 13: ['', 'SS.CHINNADURAI'], 14: ['', 'P.SENTHIL KUMAR'], 15: ['', 'TITANIC SHANMUGAM'], 16: ['', 'KS TEA STALL'], 20: ['7010147410', 'VADAKAM AKKA '], 21: ['', 'boss'], 22: ['9442054356', 'P.SENTHIL KUMAR'], 23: ['', 'VIKAS'], 24: ['', 'SB HOMES'], 25: ['', 'VARADHARAJAN AUDITOR'], 26: ['', 'RAJAGOPAL '], 27: ['9003552999', 'Sri Rengan '], 28: ['9003552999', 'Sri Rengan'], 29: ['8778064043', 'P.Dinesh'], 30: ['', 'Kathar Store'], 31: ['', 'A3J Mess'], 32: ['', 'TMB அக்கா '], 33: ['', 'R.KRISHNA NADAR'], 34: ['', 'dfgsdg']}
+    today_product_details = {}
+    """for customer_id in customer_id_phone_no_mappings:
         if customer_id_phone_no_mappings[customer_id][0] == '':
             continue
         current_customer_details = []
         current_customer_unpaid_balance = db.get_unpaid_amount(customer_id)
-        current_customer_bill_dict = db.get_unpaid_transactions(customer_id)
+        current_customer_bill_dict = db.get_unpaid_transactions(customer_id, no_of_days)
         current_customer_details.append(current_customer_unpaid_balance)
         current_customer_details.append(current_customer_bill_dict)
         customer_bill_details[customer_id] = current_customer_details"""
-
     #for owner message
     get_contact_result = await client(functions.contacts.GetContactsRequest(hash=0))
 
     # Invalid no - empty no, less than 10 digits or greater than 13 digits
-    owner_phone_no = "7010942886"
-    if not (owner_phone_no.startswith("+91")) and len(owner_phone_no) == 10:
-        owner_phone_no = "+91" + owner_phone_no
-    elif not (owner_phone_no.startswith("+")) and len(owner_phone_no) == 12:
-        owner_phone_no = "+" + owner_phone_no
+    # owner_no_fix
+    if not (owner_phone_number.startswith("+91")) and len(owner_phone_number) == 10:
+        owner_number = "+91" + owner_phone_number
+    elif not (owner_phone_number.startswith("+")) and len(owner_phone_number) == 12:
+        owner_number = "+" + owner_phone_number
+    else:
+        owner_number = owner_phone_number
     flag = 0
     for each_user in get_contact_result.users:
-        if each_user.phone == owner_phone_no[1:]:
+        if each_user.phone == owner_number[1:]:
             flag = 1
             break
     if flag == 0:
-        contact = InputPhoneContact(client_id=0, phone=owner_phone_no,
-                                    first_name=owner_name + company_string, last_name="")
+        contact = InputPhoneContact(client_id=0, phone=owner_number,
+                                    first_name=owner_name + '_' + company_string, last_name="")
         result = await client(ImportContactsRequest([contact]))
-    entity = await client.get_entity(owner_phone_no)
+    owner_number_exception = False
+    try:
+        entity = await client.get_entity(owner_number)
+    except:
+        already_exists = False
+        with open("no_telegram.txt", "r") as myfile:
+            if owner_number in myfile.read():
+                already_exists = True
+        with open("no_telegram.txt", "a") as myfile:
+            if not already_exists:
+                details = "Name: {}, Phone: {}\n".format(owner_name, owner_number)
+                myfile.write(details)
+        owner_number_exception = True
+    msg_content = "ஹலோ ! {} நாட்களுக்கு மேல் நிலுவையில் உள்ள பரிவர்த்தனைகள்\n".format(no_of_days)
     for customer_id in customer_id_phone_no_mappings:
+        if owner_number_exception:
+            continue
         bill_exists = False
         current_phone_number = customer_id_phone_no_mappings[customer_id][0]
         if current_phone_number == '' or len(current_phone_number) < 10 or len(current_phone_number) > 13:
             continue
-        msg_content = "Showing details for \nCUSTOMER_ID: {}\nCUSTOMER_NAME: {}\nCUSTOMER_CONTACTNO: {}\n\n".format(customer_id,customer_id_phone_no_mappings[customer_id][1], customer_id_phone_no_mappings[customer_id][0])
+        if customer_id in customer_bill_details and  customer_bill_details[customer_id][0] <= 0:
+            continue
+        msg_content += "\nவாடிக்கையாளர் அடையாள எண்: {}\nவாடிக்கையாளர் பெயர்: {}\nவாடிக்கையாளர் தொடர்பு எண்: {}\n\n".format(customer_id,customer_id_phone_no_mappings[customer_id][1], customer_id_phone_no_mappings[customer_id][0])
         if customer_id_phone_no_mappings[customer_id][0] == '':
             continue
         if customer_id in customer_bill_details:
-            bill_exists = True
             unpaid_balance = customer_bill_details[customer_id][0]
-            msg_content += "Your Total Unpaid Amount : {}\n\n".format(str(unpaid_balance))
-            msg_content += "Showing unpaid details transactions wise\n\n".format(str(unpaid_balance))
+            msg_content += "உங்கள் மொத்த செலுத்தப்படாத தொகை : {} Rupees\n\n".format(str(unpaid_balance))
+            msg_content += "{} நாட்களுக்கு மேல் நிலுவையில் உள்ள பரிவர்த்தனைகள் :\n\n".format(no_of_days,str(unpaid_balance))
             current_customer_bill_details = customer_bill_details[customer_id][1]
-            for each_bill in current_customer_bill_details:
+            for bill_ids, each_bill in enumerate(current_customer_bill_details):
+                bill_exists = True
                 invoice_no = each_bill
                 transaction_date = current_customer_bill_details[each_bill][0]
                 total_amount = current_customer_bill_details[each_bill][1]
                 balance_amount = current_customer_bill_details[each_bill][2]
-                msg_content += "INVOICE NO: {}\nTRANSACTION_DATE: {}\nBILL_AMOUNT: {}\nBALANCE_AMOUNT: {}\n\n".format(invoice_no, transaction_date, total_amount, balance_amount)
-        if bill_exists:
-            await client.send_message(entity, msg_content)
-
+                datediff = current_customer_bill_details[each_bill][3]
+                if len(msg_content) > content_length:
+                    time.sleep(sleep_time)
+                    await client.send_message(entity, msg_content)
+                    msg_content = ""
+                msg_content += "பில் எண்:{}\nநீங்கள் இந்த பரிவர்த்தனையை செய்து {} நாட்கள் ஆகிவிட்டன\nபரிவர்த்தனை எண்: {}\nபரிவர்த்தனை தேதி: {}\nமொத்த தொகை : {}\nமீதமுள்ள தொகை: {}\n\n".format(bill_ids +1,datediff, invoice_no, transaction_date, total_amount, balance_amount)
+            msg_content += "-----------------------------------------------------------------------\n\n"
+    import pdb;pdb.set_trace()
+    if len(msg_content) > 1:
+        time.sleep(sleep_time)
+        await client.send_message(entity, msg_content)
     #for individual messages
     for customer_id in customer_id_phone_no_mappings:
         bill_exists = False
         #Invalid no - empty no, less than 10 digits or greater than 13 digits
         current_phone_number = customer_id_phone_no_mappings[customer_id][0]
         current_name = customer_id_phone_no_mappings[customer_id][1]
-        import pdb;pdb.set_trace()
         if current_phone_number == '' or len(current_phone_number) < 10 or len(current_phone_number) > 13:
             continue
         if not(current_phone_number.startswith("+91")) and len(current_phone_number) == 10:
             current_phone_number = "+91" + current_phone_number
         elif not(current_phone_number.startswith("+")) and len(current_phone_number) == 12:
             current_phone_number = "+" + current_phone_number
-        msg_content = "Hello ! Showing your balance/transaction details\nCUSTOMER_ID: {}\nCUSTOMER_NAME: {}\nCUSTOMER_CONTACTNO: {}\n\n".format(
+        msg_content = "ஹலோ ! உங்கள் பரிவர்த்தனை விவரங்கள்\n\nவாடிக்கையாளர் அடையாள எண்: {}\n\nவாடிக்கையாளர் பெயர்: {}\n\nவாடிக்கையாளர் தொடர்பு எண்: {}\n\n".format(
             customer_id, customer_id_phone_no_mappings[customer_id][1], customer_id_phone_no_mappings[customer_id][0])
         if customer_id_phone_no_mappings[customer_id][0] == '':
             continue
         if customer_id in customer_bill_details:
             unpaid_balance = customer_bill_details[customer_id][0]
-            msg_content += "Your Total Unpaid Amount : {}\n\n".format(str(unpaid_balance))
-            msg_content += "Showing unpaid details transactions wise\n\n".format(str(unpaid_balance))
+            msg_content += "உங்கள் மொத்த செலுத்தப்படாத தொகை : {} Rupees\n\n".format(str(unpaid_balance))
+            msg_content += "நிலுவையில் உள்ள பரிவர்த்தனைகள் :\n\n".format(str(unpaid_balance))
             current_customer_bill_details = customer_bill_details[customer_id][1]
-            for each_bill in current_customer_bill_details:
+            for bill_id, each_bill in enumerate(current_customer_bill_details):
+                if len(msg_content) > content_length:
+                    time.sleep(sleep_time)
+                    await client.send_message(entity, msg_content)
+                    msg_content = ""
                 bill_exists = True
                 invoice_no = each_bill
                 transaction_date = current_customer_bill_details[each_bill][0]
                 total_amount = current_customer_bill_details[each_bill][1]
                 balance_amount = current_customer_bill_details[each_bill][2]
-                msg_content += "INVOICE NO: {}\nTRANSACTION_DATE: {}\nBILL_AMOUNT: {}\nBALANCE_AMOUNT: {}\n\n".format(
-                    invoice_no, transaction_date, total_amount, balance_amount)
+                datediff = current_customer_bill_details[each_bill][3]
+                msg_content += "பில் எண்:{}\nநீங்கள் இந்த பரிவர்த்தனையை செய்து {} நாட்கள் ஆகிவிட்டன\nபரிவர்த்தனை எண்: {}\nபரிவர்த்தனை தேதி: {}\nமொத்த தொகை : {}\nமீதமுள்ள தொகை: {}\n\n".format(bill_id +1,datediff, invoice_no, transaction_date, total_amount, balance_amount)
         get_contact_result = await client(functions.contacts.GetContactsRequest(hash=0))
         flag = 0
         for each_user in get_contact_result.users:
@@ -124,13 +173,46 @@ async def main():
                 flag = 1
                 break
         if flag == 0:
-            contact = InputPhoneContact(client_id=0, phone=owner_phone_no,
-                                        first_name= current_name + company_string, last_name="")
+            contact = InputPhoneContact(client_id=0, phone=current_phone_number,
+                                        first_name=current_name + company_string, last_name="")
             result = await client(ImportContactsRequest([contact]))
-        entity = await client.get_entity(current_phone_number)
+        current_phone_number_exception = False
+        try:
+            entity = await client.get_entity(current_phone_number)
+        except:
+            already_exists = False
+            with open("no_telegram.txt", "r") as myfile:
+                if current_phone_number in myfile.read():
+                    already_exists = True
+            with open("no_telegram.txt", "a") as myfile:
+                if not already_exists:
+                    details = "Name: {}, Phone: {}, CustomerId: {}\n".format(current_name, current_phone_number, customer_id)
+                    myfile.write(details)
+            current_phone_number_exception = True
+        if current_phone_number_exception:
+            continue
         if bill_exists:
+            time.sleep(sleep_time)
             await client.send_message(entity, msg_content)
-    """#for debugging purpose
+        msg_content = "இன்றைய விலையைக் கண்டறியவும்\n\n"
+        product_exists = False
+        for item,product in enumerate(today_product_details):
+            product_exists = True
+            if len(msg_content) > content_length:
+                time.sleep(sleep_time)
+                await client.send_message(entity, msg_content)
+                break
+                msg_content = ""
+            p_name = today_product_details[product][0]
+            p_price = today_product_details[product][1]
+            p_id = product
+            msg_content += "வரிசை எண்: {}\nதயாரிப்பு அடையாள எண்: {}\nதயாரிப்பின் பெயர்: {}\nவிலை: {}\n\n".format(item+1, p_id, p_name, p_price)
+        if product_exists:
+            time.sleep(sleep_time)
+            await client.send_message(entity, msg_content)
+
+
+    """"#for debugging purpose
     print("*******************************************************************")
     for customer in customer_bill_details:
         print(customer, customer_bill_details[customer])
